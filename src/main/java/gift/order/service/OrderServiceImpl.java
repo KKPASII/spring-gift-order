b4795a2251255154d95c2eeaca6a11485f902008
KKPASII <kkongpasi1005@gmail.com>
2025-08-01 05:09:22 +0900

    fix(order): 상품 주문 시 위시리스트 내 상품 제거 오류 해결

--- src/main/java/gift/order/service/OrderServiceImpl.java
+++ src/main/java/gift/order/service/OrderServiceImpl.java
@@ -37,7 +37,7 @@ public class OrderServiceImpl implements OrderService {
         Order newOrder = OrderMapper.toOrder(orderRequest);
         Order savedOrder = orderRepository.save(newOrder);
 
-        wishRepository.findByMemberIdAndOptionId(member.getId(), option.getProduct().getId())
+        wishRepository.findByMemberIdAndOptionId(member.getId(), option.getId())
             .ifPresent(wish -> wishRepository.deleteById(wish.getId()));
 
         sendKakaoTalkOrderConfirmation(member, savedOrder);
