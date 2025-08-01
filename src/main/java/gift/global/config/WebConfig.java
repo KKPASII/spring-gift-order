81b9a0447990368ad608f6da005befb56c03a168
Hamplz <kkongpasi1005@gmail.com>
2025-08-01 19:02:42 +0900

    fix: 카카오 redirect-uri 수정

--- src/main/java/gift/global/config/WebConfig.java
+++ src/main/java/gift/global/config/WebConfig.java
@@ -26,7 +26,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://your-client-domain.com")
+            .allowedOrigins("http://localhost:3000")
             .allowedMethods(
                 HttpMethod.GET.name(),
                 HttpMethod.POST.name(),
